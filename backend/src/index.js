67929c539d8cfa1f7645249ff544a6c01826fabb
marwenjammazi171 <https://github.com/marwenjammazi.com>
2025-05-28 17:04:58 +0100

    fix: Update CORS origin to production URL

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://chat-project-roan.vercel.app",
+    origin: "https://chat-project-virid.vercel.app",
     credentials: true,
   })
 );
