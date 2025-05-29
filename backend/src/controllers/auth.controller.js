ee6cb1f5349dc27c9919706b05dbaa483fe12ab1
marwenjammazi171 <https://github.com/marwenjammazi.com>
2025-05-29 14:08:54 +0100

    update LAX  to None

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -68,7 +68,7 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "Lax", // Use 'None' if frontend and backend are on different domains with HTTPS
+      sameSite: "None", // Use 'None' if frontend and backend are on different domains with HTTPS
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
